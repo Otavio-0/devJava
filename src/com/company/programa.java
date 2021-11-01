bc99ecd38baa3f0c2ae70f65c820bfbf80d8697e
Otavio-0 <otaviofrezzato@gmail.com>
2021-11-01 11:51:27 -0300

    first commit

---
+++ src/com/company/programa.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.model.gato;
+
+public class programa {
+
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+
+    }
+}
